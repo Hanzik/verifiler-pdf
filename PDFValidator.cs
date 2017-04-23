a4a4e7ce515c6c10b99bad0211f59c0c2a8efb12
Honza Vrátník <vratnja2@fel.cvut.cz>
2017-04-24 00:19:22 +0200

    Repository initialization

---
+++ PDFValidator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using iTextSharp.text.exceptions;
+using iTextSharp.text.pdf;
+using NLog;
+using VerifilerCore;
+
+namespace VerifilerPDF {
+
+	/// <summary>
+	/// This validation step is using the iTextSharp library for
+	/// manipulating PDF files.
+	/// 
+	/// The error code produced by this validation is Error.Corrupted.
+	/// </summary>
+	public class PDFValidator : FormatSpecificValidator {
+
+		public override int ErrorCode { get; set; } = Error.Corrupted;
+
+		private static Logger logger = LogManager.GetCurrentClassLogger();
+
+		public override void Setup() {
+			Name = "PDF files verification";
+			RelevantExtensions.Add(".pdf");
+			Enable();
+		}
+
+		public override void ValidateFile(string file) {
+
+			using (var existingFileStream = new FileStream(file, FileMode.Open)) {
+				try {
+					var pdfReader = new PdfReader(existingFileStream);
+					pdfReader.Close();
+				} catch (InvalidPdfException e) {
+					ReportAsError("File is corrupted: " + file + "; Message: " + e.Message);
+					GC.Collect();
+				}
+			}
+		}
+	}
+}
